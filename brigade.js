f05dbbebb0c8c896a7e028f743c5410a824113c6
Arun <arunramachandran15@users.noreply.github.com>
2018-10-26 16:21:12 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -3,14 +3,14 @@ const { events, Job, Group } = require('brigadier')
 events.on("push", (brigadeEvent, project) => {
     //variables
     console.log("custom 0 started push")
-      var node = new Job("Rare-runner")
+      var node = new Job("Rarerunner")
 
       // We want our job to run the stock Docker Python 3 image
       node.image = "python:3"
 
       // Now we want it to run these commands in order:
       node.tasks = [
-        "echoo amfromcontainerinsidepusheventjobhaha"
+        "echo amfromcontainerinsidepusheventjobhaha"
       ]
 
       // We're done configuring, so we run the job
@@ -21,14 +21,14 @@ events.on("push", (brigadeEvent, project) => {
 events.on("exec", (brigadeEvent, project) => {
     //variables
     console.log("custom 1 started execi ")
-    var node = new Job("Rare-runner")
+    var node = new Job("Rarerunner")
 
       // We want our job to run the stock Docker Python 3 image
       node.image = "python:3"
 
       // Now we want it to run these commands in order:
       node.tasks = [
-        "echoo amfromcontainerinsideexeceventjobhaha"
+        "echo amfromcontainerinsideexeceventjobhaha"
       ]
 
       // We're done configuring, so we run the job
@@ -41,14 +41,14 @@ events.on("myevent", (brigadeEvent, project) => {
     //variables
     console.log(" custom 2 started myevent")
     
-      var node = new Job("Rare-runner")
+      var node = new Job("rarerunner")
 
       // We want our job to run the stock Docker Python 3 image
       node.image = "python:3"
 
       // Now we want it to run these commands in order:
       node.tasks = [
-        "echoo amfromcontainerinsidemyeventjobhaha"
+        "echo amfromcontainerinsidemyeventjobhaha"
       ]
 
       // We're done configuring, so we run the job
